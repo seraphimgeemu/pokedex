4bbe5ea4610ed724033cda7b6c6143918c1651b1
SeraphimCode <seraphimgeemu@gmail.com>
2025-09-18 09:27:34 -0400

    1.2

--- pokedex_app/views.py
+++ pokedex_app/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render
 import json
 import urllib.request
+from http import HTTPStatus
+from urllib.error import HTTPError
 # Create your views here.
+
+
 def index(request):
-    if request.method == "POST":
-        pokemon = request.POST.get('pokemon').lower()
-        pokemon = pokemon.replace(" ", "%20")
-        url_pokeapi = urllib.request.Request(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-        url_pokeapi.add_header("User-Agent", "Mozilla/5.0")
+    try:    
+        if request.method == "POST":
+            pokemon = request.POST.get('pokemon').lower()
+            pokemon = pokemon.replace(" ", "%20")
+            url_pokeapi = urllib.request.Request(
+                f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+            url_pokeapi.add_header("User-Agent", "Mozilla/5.0")
 
-        source = urllib.request.urlopen(url_pokeapi).read()
-        list_data = json.loads(source)
+            source = urllib.request.urlopen(url_pokeapi).read()
+            list_data = json.loads(source)
 
-        data = {
-            "number": str(list_data["id"]),
-            "name": str(list_data["name"]).capitalize(),
-            "height": str(list_data["height"]) + " dm",
-            "weight": str(list_data["weight"]) + " hg",
-            "sprite": str(list_data["sprites"]["front_default"]),
-        }
+            list_data["height"] = str(round(float(list_data["height"]) * 0.1, 1)) + " m"
+            list_data["weight"] = str(round(float(list_data["weight"]) * 0.1, 1)) + " kg"
 
-    else:
-        data = {}
-    return render(request, "index.html", {"data": data})
+            data = {
+                "number": str(list_data["id"]),
+                "name": str(list_data["name"]).capitalize(),
+                "height": str(list_data["height"]),
+                "weight": str(list_data["weight"]),
+                "types": [str(i["type"]["name"]).capitalize() for i in list_data["types"]],
+                "sprite": str(list_data["sprites"]["front_default"]),
+            }
+            
+
+        else:
+            data = {}
+        return render(request, "index.html", {"data": data})
+    except HTTPError:
+        data = {"error": "Pokemon no encontrado, intenta de nuevo"}
+        return render(request, "index.html", {"data": data})
