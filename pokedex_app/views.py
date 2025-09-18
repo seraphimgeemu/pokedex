864b65bf7b116bd875aa38058bad017f006d7f65
SeraphimCode <seraphimgeemu@gmail.com>
2025-09-18 13:59:33 -0400

    1.4

--- pokedex_app/views.py
+++ pokedex_app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 import json
 import urllib.request
-from http import HTTPStatus
 from urllib.error import HTTPError
+
 # Create your views here.
-
-
 def index(request):
-    try:    
+    try:
         if request.method == "POST":
             pokemon = request.POST.get('pokemon').lower()
             pokemon = pokemon.replace(" ", "%20")
@@ -21,6 +19,32 @@ def index(request):
             list_data["height"] = str(round(float(list_data["height"]) * 0.1, 1)) + " m"
             list_data["weight"] = str(round(float(list_data["weight"]) * 0.1, 1)) + " kg"
 
+            traducciones = {
+                "number": "Número",
+                "name": "Nombre",
+                "height": "Altura",
+                "weight": "Peso",
+                "types": "Tipo",
+                "Fire": "Fuego",
+                "Water": "Agua",
+                "Grass": "Planta",
+                "Electric": "Eléctrico",
+                "Psychic": "Psíquico",
+                "Ice": "Hielo",
+                "Dragon": "Dragón",
+                "Dark": "Siniestro",
+                "Fairy": "Hada",
+                "Normal": "Normal",
+                "Fighting": "Lucha",
+                "Flying": "Volador",
+                "Poison": "Veneno",
+                "Ground": "Tierra",
+                "Rock": "Roca",
+                "Bug": "Bicho",
+                "Ghost": "Fantasma",
+                "Steel": "Acero"
+            }
+
             data = {
                 "number": str(list_data["id"]),
                 "name": str(list_data["name"]).capitalize(),
@@ -29,11 +53,22 @@ def index(request):
                 "types": [str(i["type"]["name"]).capitalize() for i in list_data["types"]],
                 "sprite": str(list_data["sprites"]["front_default"]),
             }
-            
+
+            data_traducido = {}
+            for clave, valor in data.items():
+                clave_traducida = traducciones.get(clave, clave)
+                if isinstance(valor, list):
+                    valor_traducido = [traducciones.get(v, v) for v in valor]
+                else:
+                    valor_traducido = traducciones.get(valor, valor)
+                data_traducido[clave_traducida] = valor_traducido
+
+            return render(request, "index.html", {"data": data_traducido})
 
         else:
             data = {}
-        return render(request, "index.html", {"data": data})
+            return render(request, "index.html", {"data": data})
+
     except HTTPError:
-        data = {"error": "Pokemon no encontrado, intenta de nuevo"}
+        data = {"error": "Pokémon no encontrado, intenta de nuevo"}
         return render(request, "index.html", {"data": data})
